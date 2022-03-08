cd1f2fb7d907287eb83fc52b8cbda2d289d9393a
Rayya <rayyatar@gmail.com>
2022-03-08 16:24:50 +0100

    add home screen that reads data from json file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import NavigationBar from './components/NavigationBar';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <Router>
       <div>
         <NavigationBar/>
+        <HomeScreen/>
         <Routes>
           {/* <Route path="/" exact component={Home}/> */}
           {/* <Route path="/dishes" component={Dishes}/>
---
+++ src/screens/HomeScreen.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import categories from '../data/catigories.json'
+
+export default function HomeScreen() {
+
+  return (
+    <div>
+        <h1>Vegado</h1>
+        <h3>Come hungry Leave Happy</h3>
+           {categories.map(category => (
+             <section key={category.id}>
+               <h1>{category.image}</h1>
+               <article>
+               <h2>{category.name}</h2>
+               <p>{category.motivation}</p>
+               <Link to={`/catigory/${category.name}`}><button>View menu</button></Link>
+               </article>
+             </section>
+           ))
+           }
+    </div>
+  )
+}
