93911f1c017b279cc15fa62348c6e6539c90cc73
Rayya <rayyatar@gmail.com>
2022-03-09 21:04:59 +0100

    edit navigation bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import logo from '../assests/logo.png'
-import { Link } from 'react-router-dom' 
+import { Link } from 'react-router-dom'
+import categories from '../data/categories.json'
 
 export default function NavigationBar() {
   return (
     <nav>
         <Link to="/"><img src={logo} alt="logo"/></Link>
         <ul>
-            <Link to="/dishes"><li>Dishes</li></Link>
-            <Link to="/deserts"><li>Deserts</li></Link>
-            <Link to="/drinks"><li>Drinks</li></Link>
+        {categories.map(category => (
+          <Link to={`${category.name}`}><li key={category.id}>{category.name}</li></Link>
+        ))}
             <Link to="/contact"><li>Contact us</li></Link>
         </ul>
     </nav>
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -14,7 +14,6 @@ export default function HomeScreen() {
                <article>
                <h2>{category.name}</h2>
                <p>{category.motivation}</p>
-               {/* <Link to={`/category/${category.name}`}><button>View menu</button></Link> */}
                <Link to={`${category.name}`}><button>View menu</button></Link>
                </article>
              </section>
