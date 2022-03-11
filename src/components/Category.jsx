14a15912aeec8eef78d4e0524eef490e07aa9056
Rayya <rayyatar@gmail.com>
2022-03-11 06:50:35 +0100

    refactoring

---
+++ src/components/Category.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Category({ item }) {
+    const { name } = item;
+  return (
+    <h1>HHHHHH{name}</h1>
+  )
+}
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import categories from '../data/categories.json'
+import Category from '../components/Category'
 
 export default function HomeScreen() {
 
+  const Projects = categories.map((item)=> <Category key={item.id} item={item}/>)
 
-  return (
-    <div className='home-screen'>
-      <div className="hero">
-        <div className="brand-container">
-        <h1>Vegado</h1>
-        <h3>Come hungry Leave healthy</h3>
-        </div>
-      </div>
-           {categories.map(category => (
-             <section className="categories-container" key={category.id}>
-               <img src={category.image} alt="category thumbnail"/>
-               <h1>{category.image}</h1>
-               <article>
-               <h2>{category.name}</h2>
-               <p>{category.motivation}</p>
-               <Link to={`${category.name}`}><button className="button-secondary">View menu</button></Link>
-               </article>
-             </section>
-           ))
-           }
-    </div>
+  return(
+    <div>{Projects}</div>
   )
+
+
+  // return (
+  //   <div className='home-screen'>
+  //     <div className="hero">
+  //       <div className="brand-container">
+  //       <h1>Vegado</h1>
+  //       <h3>Come hungry Leave healthy</h3>
+  //       </div>
+  //     </div>
+  //          {categories.map(category => (
+  //            <section className="categories-container" key={category.id}>
+  //              <img src={category.image} alt="category thumbnail"/>
+  //              <h1>{category.image}</h1>
+  //              <article>
+  //              <h2>{category.name}</h2>
+  //              <p>{category.motivation}</p>
+  //              <Link to={`${category.name}`}><button className="button-secondary">View menu</button></Link>
+  //              </article>
+  //            </section>
+  //          ))
+  //          }
+  //   </div>
+  // )
 }
