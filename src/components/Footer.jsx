3c5b966ab1f1887097b8b67fd12d5421c85bc8a2
Rayya <rayyatar@gmail.com>
2022-03-13 15:41:35 +0100

    add footer css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,31 +1,26 @@
-import React from 'react'
+//NPM Packages
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTwitter } from '@fortawesome/free-brands-svg-icons' 
-import { faInstagram } from '@fortawesome/free-brands-svg-icons' 
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
-import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
-import { faPhoneSquare } from '@fortawesome/free-solid-svg-icons'
-
+import { faTwitter,faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons' 
+import { faMapMarker, faPhoneSquare } from '@fortawesome/free-solid-svg-icons'
 
 export default function Footer() {
   return (
     <div>
         <footer>
-            {/* <article className="social-media">
+            <section className="social-media">
                 <small>Follow us</small>
                 <ul>
                     <li><FontAwesomeIcon icon={faTwitter}/></li>
                     <li><FontAwesomeIcon icon={faInstagram}/></li>
                     <li><FontAwesomeIcon icon={faFacebook}/></li>
                 </ul>
-            </article>
-            <article className="contact">
+            </section>
+            <section className="contact">
                 <ul>
-                <li><FontAwesomeIcon icon={faMapMarker} /><small>Kemivagn 7A, Gothenburg</small></li>
-                <li><FontAwesomeIcon icon={faPhoneSquare}/><small>0737786886</small></li>
+                <li><FontAwesomeIcon className="contact-icon" icon={faMapMarker} /><small>Kemivagn 7A, Gothenburg</small></li>
+                <li><FontAwesomeIcon className="contact-icon" icon={faPhoneSquare}/><small>0737786886</small></li>
                 </ul>
-            </article> */}
-            <p> &copy; {new Date().getFullYear()} &bull; Raya Altarabulsi  </p>
+            </section>
         </footer>
     </div>
   )
